cc95b445e2aa55a58670d4861a4799d05fb4a672
Peter Hosey <git@boredzo.org>
2012-11-16 19:36:15 -0800

    Initial Commit

---
+++ CodeRunner Gist adapter/PRHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRHAppDelegate.h
+//  CodeRunner Gist adapter
+//
+//  Created by Peter Hosey on 2012-11-16.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PRHAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CodeRunner Gist adapter/PRHAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  PRHAppDelegate.m
+//  CodeRunner Gist adapter
+//
+//  Created by Peter Hosey on 2012-11-16.
+//  Copyright (c) 2012 Peter Hosey. All rights reserved.
+//
+
+#import "PRHAppDelegate.h"
+
+@implementation PRHAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
